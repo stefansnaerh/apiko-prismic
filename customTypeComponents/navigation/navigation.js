777d6185200da7c84d4eece728c95023a788fe37
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-04-14 11:11:49 +0000

    fixed images

--- customTypeComponents/navigation/navigation.js
+++ customTypeComponents/navigation/navigation.js
@@ -13,7 +13,6 @@ import calendarIcon from '../../public/calendar.svg'
 import phoneIcon from '../../public/phone.png'
 
 const Navigation = ( {navigationContainer, isHamburgerOpen, setIsHamburgerOpen} ) => {
-     
     return (
         
         <header className={styles.header}>
@@ -41,13 +40,14 @@ const Navigation = ( {navigationContainer, isHamburgerOpen, setIsHamburgerOpen} 
                     <a href={'/'} >
                         <li>{navigationContainer.data.home_link_text }</li>
                     </a>
-                    <a href={Link.url(navigationContainer.data.about_link)}>
+                     {/* here im directing the link to the uid instead of using the Link from prismic*/}
+                    <a href={`/${navigationContainer.data.about_link.uid}`}>
                         <li>{navigationContainer.data.about_link_text }</li>
                     </a>
                     <a href={Link.url(navigationContainer.data.service_link)}>
                         <li>{navigationContainer.data.service_text }</li>
                     </a>
-                    <a href='/dentists'>
+                    <a href={'/dentists'}>
                         <li>{navigationContainer.data.specialists_link_text }</li>
                     </a>
                     <a href={Link.url(navigationContainer.data.contact_link)}>
--- slices/TextWithSlidingImages/index.js
+++ slices/TextWithSlidingImages/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { RichText } from 'prismic-reactjs'
 import { PrismicNextImage } from '@prismicio/next'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 import styles from './textWithSlidingImages.module.scss'
 
-const TextWithSlidingImages = ({ slice }) => (
+const TextWithSlidingImages = ({ slice }) => {
+  const [index, setIndex] = useState(0) 
+
+  return (
   <section className={styles.container}>
     <div className={styles.textContainer}>
       <RichText render={slice.primary.title}/>
@@ -13,16 +18,20 @@ const TextWithSlidingImages = ({ slice }) => (
     {slice?.items?.map((item, i) => { 
       console.log(item)
       return (
-      <>
+      <motion.div 
+      key={i}
+      animate={index === i ? {x:0} : {x:0}}
+      >
         <PrismicNextImage
           field={item.slideImage} 
           width={350}
           height={250} 
         />
-      </>
+      </motion.div>
      )})}
     </div>
   </section>
-)
+  )
+}
 
 export default TextWithSlidingImages
