87b42b6ffa7dd876c4c72400ef6eb872ab72c9e6
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-04-15 10:09:55 +0000

    slider getting ready, moving on to resposive before finishingf

--- slices/TextWithSlidingImages/index.js
+++ slices/TextWithSlidingImages/index.js
@@ -3,10 +3,30 @@ import { RichText } from 'prismic-reactjs'
 import { PrismicNextImage } from '@prismicio/next'
 import { motion } from 'framer-motion'
 import { useState } from 'react'
+import Image from 'next/image'
 import styles from './textWithSlidingImages.module.scss'
 
+import chevronRight from '../../public/chevronRight.svg'
+import chevronLeft from '../../public/chevronLeft.svg'
+
+
 const TextWithSlidingImages = ({ slice }) => {
+  // state to handle sliding images inside motion.div
   const [index, setIndex] = useState(0) 
+
+  // counting nr of images and adding 1 so it goes to 0 if the counter goes over nr of images
+  const imgCount = slice.items.length
+  console.log(imgCount)
+  const slideRight = () => {
+   if(index === imgCount){
+    setIndex(index - imgCount)
+   } else {
+    setIndex(index +1)
+   }
+
+   console.log(index)
+  }
+  console.log(slice.items)
 
   return (
   <section className={styles.container}>
@@ -15,12 +35,25 @@ const TextWithSlidingImages = ({ slice }) => {
       <RichText render={slice.primary.paragraph}/>
     </div>
     <div className={styles.imagesContainer}>
+      <div className={styles.chevronRight} onClick={slideRight}>
+        <Image
+        src={chevronRight}
+        alt='chevron right'
+        />
+      </div>
+      <div className={styles.chevronLeft} onClick={() => setIndex(-1)}>
+        <Image
+        src={chevronLeft}
+        alt='chevron right'
+        />
+      </div>
     {slice?.items?.map((item, i) => { 
       console.log(item)
       return (
       <motion.div 
       key={i}
-      animate={index === i ? {x:0} : {x:0}}
+      animate={index === i ? {x:0}  : {x:400}}
+      transition={{duration:0.5}}
       >
         <PrismicNextImage
           field={item.slideImage} 
