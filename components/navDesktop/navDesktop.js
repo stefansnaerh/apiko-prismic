d41972f993cb7ffabaa9cb3361372f5671f76ea6
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-04-21 14:23:48 +0000

    took underline nav out because of time constraints

--- components/navDesktop/navDesktop.js
+++ components/navDesktop/navDesktop.js
@@ -15,17 +15,15 @@ const NavDesktop = ( {navigationContainer, handleScrollContact} ) => {
         <nav className={styles.nav}>
             <ul>
                     <a href={'/'} >
-                        <li onClick={() => setUnderLineNav('home')}>{navigationContainer.data.home_link_text }</li>
-                        <div style={underLineNav === 'home' ? {backgroundColor : "#EBB45E"} : null}></div>
+                        <li>{navigationContainer.data.home_link_text }</li>
                     </a>
                      {/* here im directing the link to the uid instead of using the Link from prismic*/}
                     <a href={`/${navigationContainer.data.about_link.uid}`}>
-                        <li onClick={() => setUnderLineNav('about')}>{navigationContainer.data.about_link_text }</li>
-                        <div style={underLineNav === 'about' ? {backgroundColor : "#EBB45E"} : null}></div>
+                        <li>{navigationContainer.data.about_link_text }</li>
                     </a>
                     <a href={'/dentists'}>
-                    <li onClick={() => setUnderLineNav('dentists')}>{navigationContainer.data.specialists_link_text }</li>
-                    <div style={underLineNav === 'dentists' ? {backgroundColor : "#EBB45E"} : null}></div>
+                    <li>{navigationContainer.data.specialists_link_text }</li>
+                    <div></div>
                     </a>
                     {/*<a href={Link.url(navigationContainer.data.service_link)}>
                         <li>{navigationContainer.data.service_text }</li>
